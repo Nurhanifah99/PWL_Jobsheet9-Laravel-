68caf6cbae038c8eaecebc54b3b63f616d092068
Nurhanifah99 <Hanifah110498@gmail.com>
2020-04-06 09:06:56 +0700

    PWL_Jobsheet9(Laravel)

---
+++ laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Profil Saya</title>
+</head>
+
+<body>
+    <h1>Profil Saya</h1>
+    <p>Perkenalkan Nama saya NUR HANIFAH, saya seorang Mahasiswi Di Politeknik Negeri Malang
+        .</p>
+</body>
+
+</html>
